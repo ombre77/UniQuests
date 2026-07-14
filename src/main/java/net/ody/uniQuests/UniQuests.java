74707e093656a4ac952264547531c3ed10690757
Odysseus <odysseus.77@hotmail.com>
2026-07-14 16:59:44 +0200

    init

---
+++ src/main/java/net/ody/uniQuests/UniQuests.java
@@ -0,0 +1,16 @@
+package net.ody.uniQuests;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class UniQuests extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
